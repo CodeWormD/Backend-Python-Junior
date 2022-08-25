e1d9b0946f09b6fe1c0eb3278155519357d34e71
Daniil <dan.dad@bk.ru>
2022-08-25 21:26:05 +0300

    slice

---
+++ python/python/tasks/index_slice.py
@@ -0,0 +1,13 @@
+# срез индекса
+
+s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# my_collection[start:stop:step]  # старт, стоп и шаг
+print(s[:])
+print(s[::-1])
+print(s[::2])
+print(s[0::3])
+print(s[-1:])
+print(s[1:3])
+print(s[-3:])
+print(s[-3:1:-1])
+print(s[1:3])
