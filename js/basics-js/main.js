34e47cff41f4b9274fdec265a545dcd7e1846c22
Daniil <dan.dad@bk.ru>
2022-11-06 14:41:02 +0300

    js

---
+++ js/basics-js/main.js
@@ -0,0 +1,11 @@
+function firstlook(number1, number2) {
+    return number1 + number2;
+    
+}
+let result = firstlook(15, 15);
+
+let name = 'Petr';
+
+console.log(`Privet, ${name}`)
+console.log(typeof(1))
+
--- python/python/algorithms/moving_average.py
+++ python/python/algorithms/moving_average.py
@@ -1,3 +1,17 @@
 # метод скользящего среднего
 # создаётся новый массив данных, и в нём значение каждой точки высчитывается
 # как среднее арифметическое предыдущих K значений из исходного набора
+
+def moving_average(timeseries, K):
+    result = []  # Пустой список.
+    for begin_index in range(0, len(timeseries) - K + 1):
+        end_index = begin_index + K
+        # Просматриваем окно шириной K.
+        current_sum = 0
+        for v in timeseries[begin_index:end_index]:
+            current_sum += v
+        current_avg = current_sum / K
+        result.append(current_avg)
+    return result
+
+moving_average([4,3,8,1,5,6,3], 3)
